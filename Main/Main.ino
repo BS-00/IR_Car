43094e8e9a6c3c93ef1a55f13dbfc20d081ab1f4
BS-00 <107329282+BS-00@users.noreply.github.com>
2023-12-24 18:25:14 -0700

    Removed curve speed decrease

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,52 +2,59 @@
 #undef min
 #undef max
 #include <array>
+
 #include "Main.hpp"
 #include "Wheels.hpp"
 #include "SensorArray.hpp"
 
 
-std::array<float, N_SENSORS> minSensorVals;
-
 float prevError = 0,
       errorIntegral = 0,
       prevCorrection = 0;
-    
 int nDonuts = 0;
 
-
 float getError() {
-  std::array<float, N_SENSORS> avgSensorVals = getAvgSensorVals();
+  std::array<float, N_SENSORS> normSensorVals = normalizeSensorVals(getAvgSensorVals());
+
+  //Sensor Fusion
   float error = 0;
   for (int i = 0; i < N_SENSORS; i++) {
-    //Normalize sensor values (some cuttoff can occur here but hopefully it will not make a large difference)
-    float normSensorVal = std::min(std::max((avgSensorVals[i] - minSensorVals[i]) * NORM_FACTORS[i], MIN_NORM_VAL), MAX_NORM_VAL);
-    //Calaulate sensor fusion error
-    error += normSensorVal * SENSOR_WEIGHTS[i];
+    error += normSensorVals[i] * SENSOR_WEIGHTS[i];
   }
   return error;
 }
 
 
 void setup() {
-  delay(3000);
   ECE3_Init();
   Serial.begin(115200);
-  Serial.println("Callibrating...");
-  if (!initWheels() || !initSensors(minSensorVals)) exit(1);
-  Serial.println("Done callibrating.");
-  delay(5000);
-  Serial.println("Starting now.");
+  if (!initWheels() || !initSensors()) exit(1);
+
+  //Indicate done callibrating
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  awaitBump1();
+  digitalWrite(LED_PIN, LOW);
+
+  delay(500);
 }
 
 void loop() {
-  float error = getError();
-  
-  if (abs(error) > DONUT_ERR_THRESH) {
-    if (nDonuts == 1) exit(0); //Car reached the start
-    error = getError();
-  }
+  //Could be a little different because of boosting but it shouldn't make a huge difference
+  int prevLSpeed = BASE_SPEED-prevCorrection,
+      prevRSpeed = BASE_SPEED+prevCorrection;
 
+  if (getSensorSum() > DONUT_ERR_THRESH) {
+    donut(prevLSpeed, prevRSpeed);
+    //Car reached the start
+    if (nDonuts == 1) {
+      updateWheelVelocities(prevLSpeed, 0, prevRSpeed, 0);
+      exit(0); 
+    }
+    nDonuts++;
+  }
+  
+  float error = getError();
   float errorDerivative = (error - prevError);
   errorIntegral += error;
   int correction = (int)(Kp*error + Kd*errorDerivative + Ki*errorIntegral);
@@ -58,19 +65,15 @@ void loop() {
     //On a straightaway
     lSpeed += STRAIGHT_SPEED_BOOST;
     rSpeed += STRAIGHT_SPEED_BOOST;
-    correction = (int)(correction / 5); //Reduce oscillations
-  } else if (abs(correction) > CURVE_CORRECTION_THRESH) {
-    //On a curve
-    lSpeed -= CURVE_SPEED_DECREASE;
-    rSpeed -= CURVE_SPEED_DECREASE;
-  }
+    correction = (int)(correction / 4); //Reduce oscillations
+  } 
 
   //Neg error => turn right, positive error => turn left
   lSpeed -= correction;
   rSpeed += correction;
   
-  updateWheelVelocities(BASE_SPEED-prevCorrection, lSpeed, 
-                        BASE_SPEED+prevCorrection, rSpeed, 2, 10);
+  updateWheelVelocities(prevLSpeed, lSpeed, 
+                        prevRSpeed, rSpeed, 2, 10);
   prevError = error;
   prevCorrection = correction;
 }
