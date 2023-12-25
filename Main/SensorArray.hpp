dfbd1a74cb0dd4c4a8b0c4a0e247533dc60dd72f
BS-00 <107329282+BS-00@users.noreply.github.com>
2023-12-24 18:27:51 -0700

    Added bump detection & tweaked norm vals

--- Main/SensorArray.hpp
+++ Main/SensorArray.hpp
@@ -1,6 +1,8 @@
 #ifndef SENSORARRAY_H
 #define SENSORARRAY_H
 
+#undef min
+#undef max
 #include <array>
 
 const int N_SENSORS = 8;
@@ -8,10 +10,21 @@ const int SENSOR_WEIGHTS[] = {-15, -14, -12, -8, 8, 12, 14, 15};
 
 const float MAX_NORM_VAL = 1000,
             MIN_NORM_VAL = 0,
-            NORM_FACTORS[] = {0.55, 0.55, 0.55, 0.55, 0.55, 0.55, 0.55, 0.55};
+            NORM_FACTORS[] = {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
+
+const int BUMP1_PIN = 24;
+
+const int DARK_THRESHOLD = 1600,
+          BRIGHT_BUFFER = 200;
 
 
-int initSensors(std::array<float, N_SENSORS>&);
-std::array<float, N_SENSORS> getAvgSensorVals(const int=2, const int=10);
+static std::array<float, N_SENSORS> minSensorVals;
+
+
+int initSensors();
+int getSensorSum();
+std::array<float, N_SENSORS> getAvgSensorVals(const int=2, const int=5, const bool=true);
+std::array<float, N_SENSORS> normalizeSensorVals(const std::array<float, N_SENSORS>&);
+int awaitBump1();
 
 #endif
