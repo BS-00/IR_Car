e34e8fba96a8972f7bd5f56e97b34237ea2a152e
BS-00 <107329282+BS-00@users.noreply.github.com>
2023-12-25 15:13:48 -0700

    Converted into SensorArray class

--- Main/SensorArray.cpp
+++ Main/SensorArray.cpp
@@ -7,58 +7,39 @@
 #include "Main.hpp"
 
 
-//Private helper functions
-//Checks if any of the middle sensors are not bright
-bool onLine(const std::array<float, N_SENSORS>& sensorVals) {
-  float rightMidSensorIndex = N_SENSORS/2;
-  int BUFFER = 200;
-  if ((int)rightMidSensorIndex == rightMidSensorIndex && rightMidSensorIndex+1 < N_SENSORS) {
-    return sensorVals[rightMidSensorIndex-2] > BRIGHT_THRESH-BUFFER || 
-           sensorVals[rightMidSensorIndex-1] > BRIGHT_THRESH-BUFFER || 
-           sensorVals[rightMidSensorIndex]   > BRIGHT_THRESH-BUFFER ||
-           sensorVals[rightMidSensorIndex+1] > BRIGHT_THRESH-BUFFER;
+//Required definitions until C++17
+constexpr std::array<int, SensorArray::N_SENSORS> SensorArray::SENSOR_WEIGHTS;
+constexpr std::array<float, SensorArray::N_SENSORS> SensorArray::NORM_FACTORS;
+
+
+SensorArray::SensorArray() : _initialized(false) {}
+
+int SensorArray::init() {
+  pinMode(_BUMP0_PIN, INPUT_PULLUP);
+
+  minSensorVals = avgSensorVals(10, 25);
+
+  //Ensure callibrated properly
+  for (float minSensorVal : minSensorVals) {
+    if (minSensorVal > _BRIGHT_THRESH) return 0;
   }
-  //Car does not have enough sensors or has an odd number of sensors
-  exit(1);
-}
-
-//Returns a boolean array containing if an obstacle was detected on each side of the car [left, right]
-std::array<bool, 2> detectObstacles(const std::array<float, N_SENSORS>& sensorVals) {
-  if (!onLine(sensorVals)) return {{0, 0}};
-
-  return {{sensorVals[0] > DARK_THRESH && sensorVals[2] < BRIGHT_THRESH, 
-           sensorVals[N_SENSORS-1] > DARK_THRESH && sensorVals[N_SENSORS-3] < BRIGHT_THRESH}};
-}
-
-std::array<float, N_SENSORS> normalizeSensorVals(const std::array<float, N_SENSORS>& sensorVals) {
-  std::array<float, N_SENSORS> normSensorVals;
-  for (int i = 0; i < N_SENSORS; i++) {
-    float normVal = (sensorVals[i] - minSensorVals[i]) * NORM_FACTORS[i];
-    normSensorVals[i] = constrain(normVal, MIN_NORM_VAL, MAX_NORM_VAL);
-  }
-  return normSensorVals;
-}
-
-
-//Public sensor array functions
-int initSensors() {
-  pinMode(BUMP1_PIN, INPUT_PULLUP);
-
-  minSensorVals = getAvgSensorVals(10, 25);
-  for (int i = 0; i < N_SENSORS; i++) {
-      if (minSensorVals[i] > 900) {
-          Serial.println("Minimum sensor values are too high, likely the car was not callibrated properly: ");
-          for (int sensorVal : minSensorVals) {
-              Serial.print(sensorVal);
-              Serial.print("\t");
-          }
-          return 0;
-      }
-  }
+  _initialized = true;
   return 1;
 }
 
-std::array<float, N_SENSORS> getAvgSensorVals(const int nSamples, const int delayMillis, const bool removeOuterSensors) {
+int SensorArray::awaitBump(int switchIndex) {
+  int switchPin;
+  switch(switchIndex) {
+    case 0 : switchPin = _BUMP0_PIN; break;
+    default: return 0; break;
+  }
+  while (digitalRead(switchPin)) delay(10);
+
+  return 1;
+}
+
+
+std::array<float, SensorArray::N_SENSORS> SensorArray::avgSensorVals(const int nSamples, const int delayMillis, const bool removeOuterSensors) const {
   std::array<float, N_SENSORS> avgSensorVals = {};
   //Average sensor values
   for (int i = 0; i < nSamples; i++) {
@@ -95,14 +76,54 @@ std::array<float, N_SENSORS> getAvgSensorVals(const int nSamples, const int dela
   return avgSensorVals;
 }
 
-int getSensorSum() {
+int SensorArray::error() const {
+  std::array<float, N_SENSORS> _normSensorVals = normSensorVals();
+
+  //Sensor Fusion
+  float error = 0;
+  for (int i = 0; i < N_SENSORS; i++) {
+    error += _normSensorVals[i] * SENSOR_WEIGHTS[i];
+  }
+  return error;
+}
+
+int SensorArray::sensorSum() const {
   float sum = 0;
-  std::array<float, N_SENSORS> avgSensorVals = getAvgSensorVals(1, 0, false);
-  for (float sensorVal : avgSensorVals) sum += sensorVal;
+  for (float sensorVal : avgSensorVals(1, 0, false)) sum += sensorVal;
   return sum;
 }
 
-int awaitBump1() {
-  while (digitalRead(BUMP1_PIN)) delay(10);
-  return 1;
+
+//Helper Functions
+
+//Checks if any of the middle sensors are not bright
+bool SensorArray::onLine(const std::array<float, N_SENSORS>& sensorVals) const {
+  float rightMidSensorIndex = N_SENSORS/2;
+  const int BUFFER = 300;
+  if ((int)rightMidSensorIndex == rightMidSensorIndex && rightMidSensorIndex+1 < N_SENSORS) {
+    return sensorVals[rightMidSensorIndex-2] > _BRIGHT_THRESH-BUFFER || 
+           sensorVals[rightMidSensorIndex-1] > _BRIGHT_THRESH || 
+           sensorVals[rightMidSensorIndex]   > _BRIGHT_THRESH ||
+           sensorVals[rightMidSensorIndex+1] > _BRIGHT_THRESH-BUFFER;
+  }
+  //Car does not have enough sensors or has an odd number of sensors
+  exit(1);
+}
+
+//Returns a boolean array containing if an obstacle was detected on each side of the car [left, right]
+std::array<bool, 2> SensorArray::detectObstacles(const std::array<float, N_SENSORS>& sensorVals) const {
+  if (!onLine(sensorVals)) return {{0, 0}};
+
+  return {{sensorVals[0] > _DARK_THRESH && sensorVals[2] < _BRIGHT_THRESH, 
+           sensorVals[N_SENSORS-1] > _DARK_THRESH && sensorVals[N_SENSORS-3] < _BRIGHT_THRESH}};
+}
+
+std::array<float, SensorArray::N_SENSORS> SensorArray::normSensorVals() const {
+  std::array<float, N_SENSORS> sensorVals = avgSensorVals(),
+                               normSensorVals;
+  for (int i = 0; i < N_SENSORS; i++) {
+    float normVal = (sensorVals[i] - minSensorVals[i]) * NORM_FACTORS[i];
+    normSensorVals[i] = constrain(normVal, MIN_NORM_VAL, MAX_NORM_VAL);
+  }
+  return normSensorVals;
 }
--- Main/SensorArray.hpp
+++ Main/SensorArray.hpp
@@ -5,26 +5,36 @@
 #undef max
 #include <array>
 
-const int N_SENSORS = 8;
-const int SENSOR_WEIGHTS[] = {-15, -14, -12, -8, 8, 12, 14, 15};
 
-const float MAX_NORM_VAL = 1000,
-            MIN_NORM_VAL = 0,
-            NORM_FACTORS[] = {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
+class SensorArray {
+public:
+    static constexpr int N_SENSORS = 8;
+    static constexpr std::array<int, N_SENSORS> SENSOR_WEIGHTS = {{-15, -14, -12, -8, 8, 12, 14, 15}};
+    static constexpr std::array<float, N_SENSORS> NORM_FACTORS = {{0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5}};
+    static constexpr float MAX_NORM_VAL = 1000,
+                           MIN_NORM_VAL = 0;
 
-const int BUMP1_PIN = 24;
+    std::array<float, N_SENSORS> minSensorVals;
 
-const int DARK_THRESH   = 1700, //Raw sensor values greater than this are considered dark
-          BRIGHT_THRESH = 1100; //Raw sensor values less than this are considered bright
+    SensorArray();
+    static int awaitBump(int);
 
+    int init();
+    int sensorSum() const;
+    int error() const;
+    std::array<float, N_SENSORS> avgSensorVals(const int=2, const int=5, const bool=true) const;
 
-static std::array<float, N_SENSORS> minSensorVals;
+private:
+    static constexpr int _BUMP0_PIN = 24;
+    static constexpr int _DARK_THRESH = 1800, 
+                         _BRIGHT_THRESH = 1200; 
+    
+    bool _initialized;
 
-
-int initSensors();
-int getSensorSum();
-std::array<float, N_SENSORS> getAvgSensorVals(const int=2, const int=5, const bool=true);
-std::array<float, N_SENSORS> normalizeSensorVals(const std::array<float, N_SENSORS>&);
-int awaitBump1();
+    std::array<float, N_SENSORS> normSensorVals() const;
+    bool onLine(const std::array<float, N_SENSORS>&) const;
+    std::array<bool, 2> detectObstacles(const std::array<float, N_SENSORS>&) const;
+    std::array<float, N_SENSORS> normalizeSensorVals(const std::array<float, N_SENSORS>&) const;
+};
 
 #endif
